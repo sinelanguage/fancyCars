8cc27a7510cf97d7c211694f7b64c806edd7b630
sinelanguage <sinelanguage@gmail.com>
2018-03-21 17:58:11 -0400

    modified default express setup to render views strictly for independent server setup confirmation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./app.css";
 import getDummyUsers from "./services/dummy-users";
 
 class App extends Component {
@@ -8,6 +8,8 @@ class App extends Component {
     users: []
   };
   componentDidMount() {
+    // getDummyUsers service uses the fetch api which returns a promise by default,
+    // exceptions are handled in the service.
     getDummyUsers().then(users =>
       this.setState({
         users
@@ -26,9 +28,9 @@ class App extends Component {
     const { users } = this.state;
     return (
       <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1 className="app-title">Welcome to React</h1>
         </header>
         <ul>{users.map(user => this.renderUser(user))}</ul>
       </div>
