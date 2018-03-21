29b8788d784b79f826cbcbf57d2568fb29bbac32
sinelanguage <sinelanguage@gmail.com>
2018-03-21 18:25:17 -0400

    setup testing adapters for chai, enzyme, sinon and react-16

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { shallow, mount, render } from "enzyme";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+import chai, { expect } from "chai";
+import chaiEnzyme from "chai-enzyme";
+
+chai.use(chaiEnzyme());
+
 import App from "./app";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("can add two numbers!", () => {
+  expect(1 + 1).to.equal(2);
 });
