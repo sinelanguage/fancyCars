ff32ed80a92dc0274be1e4c79727f11b3bb883cd
sinelanguage <sinelanguage@gmail.com>
2018-03-21 16:51:23 -0400

    Added dummy data to users demo route to wire up some data to the front end

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.json([
+    {
+      id: 1,
+      name: "Adam"
+    },
+    {
+      id: 2,
+      name: "Sylvia"
+    },
+    {
+      id: 3,
+      name: "Cassia"
+    },
+    {
+      id: 4,
+      name: "Roxy"
+    }
+  ]);
 });
 
 module.exports = router;
