e2754b39242682791eba133b577b5716097aebff
sinelanguage <sinelanguage@gmail.com>
2018-03-22 19:46:14 -0400

    updated mock data, added availability route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,26 +1,10 @@
 var express = require("express");
 var router = express.Router();
+var dummyData = require("../mock.data");
 
 /* GET cars listing. */
-router.get("/cars", function(req, res, next) {
-  res.json([
-    {
-      id: 1,
-      name: "Adam"
-    },
-    {
-      id: 2,
-      name: "Sylvia"
-    },
-    {
-      id: 3,
-      name: "Cassia"
-    },
-    {
-      id: 4,
-      name: "Roxy"
-    }
-  ]);
+router.get("/", function(req, res, next) {
+  res.json(dummyData.inventory);
 });
 
 module.exports = router;
