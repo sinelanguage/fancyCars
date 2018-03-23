57d8ea239f54dd1601e605dd83186d5668ceac6e
sinelanguage <sinelanguage@gmail.com>
2018-03-23 13:20:25 -0400

    used availbility to display availbility status, this is mock for now, with parse object properly in next commit

--- client/src/redux/actions/cars.thunk.js
+++ client/src/redux/actions/cars.thunk.js
@@ -6,19 +6,21 @@ import {
 import getCars from "../../services/cars.service";
 import getAvailability from "../../services/availbility.service";
 
+function showAvailabilityStatus(inventory) {
+  return inventory.map(car => ({
+    ...car,
+    ...getAvailability(car.id)
+  }));
+}
+
 export function fetchCars() {
   return dispatch => {
     dispatch(fetchCarsRequest());
     return getCars()
       .then(inventory => {
-        dispatch(fetchCarsSuccess(inventory));
-        return inventory;
-      })
-      .then(inventory => {
-        return inventory.map(car => ({
-          ...car,
-          availability: getAvailability(car.id)
-        }));
+        const withAvailabilityStatus = showAvailabilityStatus(inventory);
+        dispatch(fetchCarsSuccess(withAvailabilityStatus));
+        return withAvailabilityStatus;
       })
       .catch(error => dispatch(fetchCarsError(error)));
   };
