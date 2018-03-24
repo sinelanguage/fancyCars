54265916ad4e611456a8806b912417f545bbf5ee
sinelanguage <sinelanguage@gmail.com>
2018-03-24 15:28:28 -0400

    removed console logs and added getuikit.com uikit

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,12 @@ import "./index.css";
 import App from "./app";
 import registerServiceWorker from "./registerServiceWorker";
 import store from "./store";
+import "../node_modules/uikit/dist/css/uikit.min.css";
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+
+// loads the Icon plugin
+UIkit.use(Icons);
 
 ReactDOM.render(
   <Provider store={store}>
