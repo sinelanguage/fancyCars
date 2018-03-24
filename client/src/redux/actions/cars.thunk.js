844b9900f4ebc3c010869d336ca7cf23d4921da4
sinelanguage <sinelanguage@gmail.com>
2018-03-24 14:47:28 -0400

    created thunk using the cars API to get the array of cars, and then the availability API to get their availability and then created an array of new car objects that have availability key on spread onto the new cars object

--- client/src/redux/actions/cars.thunk.js
+++ client/src/redux/actions/cars.thunk.js
@@ -6,22 +6,26 @@ import {
 import getCars from "../../services/cars.service";
 import getAvailability from "../../services/availbility.service";
 
-function showAvailabilityStatus(inventory) {
-  return inventory.map(car => ({
-    ...car,
-    ...getAvailability(car.id)
-  }));
-}
-
 export function fetchCars() {
   return dispatch => {
     dispatch(fetchCarsRequest());
-    return getCars()
-      .then(inventory => {
-        const withAvailabilityStatus = showAvailabilityStatus(inventory);
-        dispatch(fetchCarsSuccess(withAvailabilityStatus));
-        return withAvailabilityStatus;
-      })
-      .catch(error => dispatch(fetchCarsError(error)));
+    return (
+      getCars()
+        .then(inventory => {
+          // make an array of promises with new car objects with availability
+          // from the second api call
+          const carPromises = inventory.map(car =>
+            getAvailability(car.id).then(avail => ({ ...car, ...avail }))
+          );
+          // resolve all promises in the array to their values and dispatch
+          return Promise.all(carPromises).then(res =>
+            dispatch(fetchCarsSuccess(res))
+          );
+        })
+        /* .then(inventory => {
+        return dispatch(fetchCarsSuccess(inventory));
+      }) */
+        .catch(error => dispatch(fetchCarsError(error)))
+    );
   };
 }
